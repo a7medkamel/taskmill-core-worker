d47c9f04b63062c93c8f0abd5680faaafd40a77e
ahmed kamel <public@ahmedkamel.net>
2015-05-17 09:34:55 +0800

    rename agent to worker

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var argv  = require('minimist')(process.argv.slice(2))
-  , Agent = require('./lib/agent')
-  , port  = argv.port || 80
+var argv    = require('minimist')(process.argv.slice(2))
+  , Worker  = require('./lib/worker')
+  , port    = argv.port || 80
   ;
 
 process.on('uncaughtException', function (err) {
-  console.error('taskmill-core-agent::uncaughtException', err.stack || err.toString());
+  console.error('taskmill-core-worker::uncaughtException', err.stack || err.toString());
 });
 
 function main(options) {
   options = options || {};
 
-  (new Agent()).listen({ port : options.port || port })
+  (new Worker()).listen({ port : options.port || port })
 }
 
 if (require.main === module) {
