c63f5ad801e843eab1c06a735319d279809a0713
Ahmed Kamel <ak@ahmedkamel.net>
2016-01-26 00:16:22 -0800

    remove build code from the core worker repo

--- docker/build.sh
+++
@@ -1,6 +0,0 @@
-cp -rf ../../taskmill-code-arbitrary ../node_modules/
-
-docker build --pull=true -t a7medkamel/taskmill-core-worker -f ./Dockerfile ../
-
-# clean up
-# docker rmi $(docker images -f "dangling=true" -q)
