1c58217fb519c6fadf14388a26eba2cc9da59548
Ahmed Kamel <ak@ahmedkamel.net>
2016-02-13 01:59:14 -0800

    refactoring

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var s_hrtime    = process.hrtime()
 console.log('>> started');
 
 (new Worker()).listen(undefined, function(err, res){
+  if (err) {
+    throw err;
+  }
+  
   var diff = process.hrtime(s_hrtime);
   console.log('>> code loaded,', ((diff[0] * 1e9 + diff[1]) / 1e6).toFixed(2) + 'ms');
 });
