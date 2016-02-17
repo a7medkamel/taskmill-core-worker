23644934dcc3a525f36bbaf63f6f8f7460eb05f3
Ahmed Kamel <ak@ahmedkamel.net>
2016-02-16 23:15:38 -0800

    pretty print err

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ var s_hrtime    = process.hrtime()
   , Worker      = require('./lib/worker')
   ;
 
+process.on('uncaughtException', function (err) {
+  console.error(new Date().toUTCString(), 'uncaughtException', err.message);
+  console.error(err.stack);
+
+  process.exit(1);
+});
+
 console.log('>> started');
 
 (new Worker()).listen(undefined, function(err, res){
